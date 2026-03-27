01ea08b5a7f175d9eb599a3bb30655152323ab2e
preetush bhowmik <157053286+PreetushBhowmik@users.noreply.github.com>
2026-03-27 23:06:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json['message']
+
+    # Temporary bot reply (later LLM will come here)
+    bot_reply = "You said: " + user_message
+
+    return jsonify({"reply": bot_reply})
+
+if __name__ == "__main__":
+    app.run(debug=True)
